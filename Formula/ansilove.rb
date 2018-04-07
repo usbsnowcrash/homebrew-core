da6da40fb0dff377eb43341ef6d2261456146693
Jeffery Yeary <usbsnowcrash@users.noreply.github.com>
2018-04-07 17:20:25 -0400

    Update homepage to the C library
    ...not the php library it was ported from

--- Formula/ansilove.rb
+++ Formula/ansilove.rb
@@ -1,6 +1,6 @@
 class Ansilove < Formula
   desc "ANSi / ASCII art to PNG converter in C"
-  homepage "https://www.ansilove.org"
+  homepage "https://github.com/ansilove/ansilove"
   url "https://github.com/ansilove/ansilove/archive/3.0.8.tar.gz"
   sha256 "655472d31a9ec5a5ba29f5c15485d8647526ff8b0c9968d71f4267a9aa9bb544"
   depends_on "cmake" => :build
